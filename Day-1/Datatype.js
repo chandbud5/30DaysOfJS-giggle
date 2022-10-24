2b21aecbfb9c796f713150265c69b10db1193857
Chand Bud <chandbud5@gmail.com>
2022-10-24 20:51:05 +0530

    Update Datatype.js

--- Day-1/Datatype.js
+++ Day-1/Datatype.js
@@ -9,3 +9,13 @@ console.log(typeof age)
 console.log(typeof isBool)
 console.log(typeof _var)
 console.log(typeof anything)
+
+let a,b,c,d;
+let A=22, B='B', C=c, D=0;
+
+let fName='Chand';
+let lName='Bud';
+let country='India', Age=21;
+
+let myAge = 21, yourAge = 31;
+console.log(`I am ${myAge} years old. You are ${yourAge} years old.`)
