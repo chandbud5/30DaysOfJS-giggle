daeb1fa931977fc1de8a9843165d93bfa92821cd
Chand Bud <chandbud5@gmail.com>
2022-10-24 19:51:26 +0530

    Create Variable.js

---
+++ Day-1/Variable.js
@@ -0,0 +1,15 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/*
+comments can make code readable, easy to reuse and informative
+*/
+
+let name = "Chand";
+let age = 21;
+let isBool = false;
+let _var;
+let anything = null;
+
+console.log(name, age, isBool, _var, anything );
