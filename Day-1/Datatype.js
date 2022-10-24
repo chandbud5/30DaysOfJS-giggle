1c4bdc4c9f43b0246449d46b8a42071ffe15bcae
Chand Bud <chandbud5@gmail.com>
2022-10-24 19:54:03 +0530

    Create Datatype.js

---
+++ Day-1/Datatype.js
@@ -0,0 +1,11 @@
+let name = "Chand";
+let age = 21;
+let isBool = false;
+let _var;
+let anything = null;
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isBool)
+console.log(typeof _var)
+console.log(typeof anything)
