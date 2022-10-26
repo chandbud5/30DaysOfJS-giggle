96abc8f839350b89a838642cdceb7a33f9e3bebc
Chand Bud <chandbud5@gmail.com>
2022-10-26 18:47:10 +0530

    Create Level3.js

---
+++ Day-3/Level3.js
@@ -0,0 +1,10 @@
+let d = new Date();
+
+let year = d.getFullYear();
+let month = d.getMonth();
+let date = d.getDate();
+let hour = d.getHours();
+let min = d.getMinutes();
+
+
+console.log(`${date}-${month}-${year} ${hour}:${min}`);
